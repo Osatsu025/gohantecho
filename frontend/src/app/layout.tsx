5931df8ec8e1847f339391567fd780dc6ade75b0
bestlife.osatsu <osatsu025@gmail.com>
2025-09-03 10:52:42 +0900

    feat: bodyの位置を調整

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex justify-center mt-8`}
       >
         {children}
       </body>
